86e604af4fa073181f5d7acbb7db8882ff926ba4
Daniele <steelturtle@msn.com>
2017-07-22 14:29:44 +0900

    Added basic Comparator logic

--- src/org/gorillacorp/comparator_example/Grid.java
+++ src/org/gorillacorp/comparator_example/Grid.java
@@ -3,6 +3,7 @@ package org.gorillacorp.comparator_example;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Grid {
@@ -39,6 +40,24 @@ public class Grid {
 	public Collection<Element> getElements() {
 		return this.elements;
 	}
+
+	// Suppose we want to compare grid elements not in natural order (as we are
+	// doing now with the Comparable interface
+	// implemented by the Element class), but using some customized behavior. Then,
+	// we can instantiate a Comparable and
+	// define our criteria for the comparison...
+	// Work in Progress: Need to improve the comparator logic, as at the moment IT
+	// IS NOT CONSISTENT
+	// WITH JAVA STANDARD RECOMMENDATION
+	static final Comparator<Element> WEIGHT_ORDER = (e1, e2) -> {
+		if (e1.getWeight() < e2.getWeight()) {
+			return -1;
+		} else if (e1.getWeight() > e2.getWeight()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	};
 
 	public boolean fillEmptyElementInGrid(String desiredElementPosition) {
 		Element chosenElement = new Element(desiredElementPosition, 0);
--- src/org/gorillacorp/comparator_example/MainApp.java
+++ src/org/gorillacorp/comparator_example/MainApp.java
@@ -1,17 +1,35 @@
 package org.gorillacorp.comparator_example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		Grid grid = new Grid("Custom Grid", 26, 12);
 		// Just for debug purposes....
-		grid.printAllElements();
+		// grid.printAllElements();
 		if (grid.fillEmptyElementInGrid("B05")) {
 			System.out.println("Success: the element has been inserted in the grid");
 		} else {
 			System.out.println("Failure: The element in the grid has already been filled");
 		}
 
+		System.out.println("**********************************");
+
+		// Let's use the Comparator<Element> defined in the Grid class... We start
+		// building a copy
+		// of the ArrayList created by the Grid constructor at the beginning of this
+		// main function up here...
+		List<Element> elementsComparedByWeight = new ArrayList<>(grid.getElements());
+		elementsComparedByWeight.add(new Element("F09", 14.00d));
+		elementsComparedByWeight.add(new Element("I08", 4.00d));
+		elementsComparedByWeight.add(new Element("E10", 9.00d));
+		elementsComparedByWeight.add(new Element("S07", 35.00d));
+
+		Collections.sort(elementsComparedByWeight, Grid.WEIGHT_ORDER);
+		grid.printAllElements();
 	}
 
 }
