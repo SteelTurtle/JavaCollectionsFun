70bf9d52d2cab621fea381bad9ad4ec863d1832a
Daniele <steelturtle@msn.com>
2017-09-30 15:29:18 +0900

    Added equals() and hashCode() methods for correct Set<> comparisons

--- src/org/gorillacorp/sets_and_hashsets/HeavenlyBody.java
+++ src/org/gorillacorp/sets_and_hashsets/HeavenlyBody.java
@@ -30,4 +30,44 @@ public final class HeavenlyBody {
 	public boolean addMoon(HeavenlyBody moon) {
 		return this.satellites.add(moon);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(revolutionPeriod);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result
+				+ ((satellites == null) ? 0 : satellites.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HeavenlyBody other = (HeavenlyBody) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(revolutionPeriod) != Double
+				.doubleToLongBits(other.revolutionPeriod))
+			return false;
+		if (satellites == null) {
+			if (other.satellites != null)
+				return false;
+		} else if (!satellites.equals(other.satellites))
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/org/gorillacorp/sets_and_hashsets/MainApp.java
+++ src/org/gorillacorp/sets_and_hashsets/MainApp.java
@@ -27,18 +27,41 @@ public class MainApp {
 		solarSystem.put(venus.getName(), venus);
 		planets.add(venus);
 
+		HeavenlyBody mars = new HeavenlyBody("Mars", 225);
+		solarSystem.put(mars.getName(), mars);
+		planets.add(mars);
+
+		// add moons to planets
 		HeavenlyBody earthMoon = new HeavenlyBody("Moon", 27);
 		solarSystem.put(earthMoon.getName(), earthMoon);
 		earth.addMoon(earthMoon);
+
+		HeavenlyBody deimos = new HeavenlyBody("Deimos", 67);
+		HeavenlyBody phobos = new HeavenlyBody("Phobos", 45);
+		solarSystem.put(deimos.getName(), deimos);
+		solarSystem.put(phobos.getName(), phobos);
+		mars.addMoon(deimos);
+		mars.addMoon(phobos);
 
 		System.out.println("Planets: ");
 		for (HeavenlyBody planet : planets) {
 			System.out.println("\t" + planet.getName());
 		}
 
-		HeavenlyBody body = solarSystem.get("Earth");
-		System.out.println("Moons of " + body.getName());
-		for (HeavenlyBody moon : body.getSatellites()) {
+		HeavenlyBody body1 = solarSystem.get("Earth");
+		System.out.println("Moons of " + body1.getName());
+		for (HeavenlyBody moon : body1.getSatellites()) {
+			System.out.println("\t" + moon.getName());
+		}
+
+		// Sets Union
+		Set<HeavenlyBody> moonsUnion = new HashSet<>();
+		for (HeavenlyBody planet : planets) {
+			moonsUnion.addAll(planet.getSatellites());
+		}
+		System.out.println(
+				"List of all moons in the (lazy version) of the solar system:");
+		for (HeavenlyBody moon : moonsUnion) {
 			System.out.println("\t" + moon.getName());
 		}
 
